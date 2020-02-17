5e2377817faf50ab2cec46d18d86a4fe83244a6c
Samir166 <38209884+Samir166@users.noreply.github.com>
2020-02-17 19:13:40 +0400

    Add files via upload

---
+++ php form/form.php
@@ -0,0 +1,53 @@
+<?php
+
+$users = [["username" => "samir" , "password" => "samir166"] ,
+		["username" => "melikmemmed" , "password" => "melikmemmed"]
+];
+
+
+  if($_SERVER['REQUEST_METHOD'] == "POST"){
+
+	if(empty($_POST["username"]) && empty($_POST["password"])){
+		echo "Melumat daxil edin";	
+	}
+	else if(empty($_POST["username"])){
+		echo "Username daxil edin";	
+	}
+	else if(empty($_POST["password"])){
+		echo "Shifre daxil edin";	
+	}
+    else{
+foreach ($users as $value) {
+	if($_POST["username"] == $value["username"] && $_POST["password"] == $value["password"]){
+		header('Location:profile.php');
+	}
+	else{
+		echo "Melumatlar sehvdir";
+		break;
+	}
+
+	}
+}
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+</head>
+
+<body>
+	
+	<form action="#" method="POST">
+		<input name="username" type="text" placeholder="Username"> <br> <br>
+		<input name="password" type="password" placeholder="Password"> <br> <br>
+		<input name="submit" type="submit" value="Submit">
+	</form>
+
+
+</body>
+
+</html>
